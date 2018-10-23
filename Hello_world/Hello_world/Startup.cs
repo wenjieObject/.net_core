5efbc23891ac1c9126caa6535c7e34d6954ffd40
WENJIE\jwj <connyoursis@163.com>
2018-10-23 22:28:25 +0800

    ASP.NET Core 项目配置 ( Startup )

--- Hello_world/Hello_world/Startup.cs
+++ Hello_world/Hello_world/Startup.cs
@@ -1,22 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Hello_world
 {
+
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
+        public IConfiguration Configuration { get; set; }
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("AppSettings.json");
+            Configuration = builder.Build();
+        }
+            
+        //定义应用程序所需的服务
+        // This method gets called by the runtime. 
+        // Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
+        //定义请求管道中的中间件
+        //例如index静态页面或者错误页面等
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
@@ -28,7 +44,9 @@ namespace Hello_world
             app.Run(async (context) =>
             {
                 //await context.Response.WriteAsync("Hello World!");
-                await context.Response.WriteAsync("Hello World!\nHello .net_core入门教程");
+                //await context.Response.WriteAsync("Hello World!\nHello .net_core入门教程");
+                var msg = Configuration["message"];
+                await context.Response.WriteAsync(msg);
 
             });
         }
